29244cc3a066fae84a906e0f3b78e4625b401f32
Pravin Patekar <125630602+PravinSimbaQuartz@users.noreply.github.com>
2024-04-25 12:53:47 +0530

    updated code

---
+++ string.js
@@ -0,0 +1,43 @@
+const options = "      ASDFGHJasdfghjklkjhgfdfghjk    asdfghjklkjhgfds    "
+
+// console.log(options.length)
+// const options3 = options.length
+// console.log('option3', options3)
+// console.log(options.charAt(19))
+// console.log(options.toLocaleLowerCase())
+// console.log(options.toLocaleUpperCase())
+// console.log(options.trim())
+
+
+
+let text1 = "Hello";
+let text2 = "World";
+
+const text4 = text1 + " " + text2
+console.log(text4)
+
+// let text3 = text2.concat(" ", text1);
+
+// console.log(text3)
+
+// let text = "Please visit Microsoft!";
+// let newText = text.replace("Microsoft", "W3Schools");
+
+
+// console.log(newText)
+
+
+
+
+// let text = "I love cats. Cats are very easy to love. Cats are very popular."
+// const text5 = text.replaceAll("Cats", "Dogs");
+// const text6 = text.replaceAll("cats", "dogs");
+
+// console.log(text5)
+// console.log(text6)
+
+
+// datatypes in Js
+
+
+
