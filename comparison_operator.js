5640972b6e02636f75dae70ebbe88c41d79b5238
Pravin Patekar <pravinpatekar1999@gmail.com>
2024-05-02 13:05:49 +0530

    updated code

---
+++ comparison_operator.js
@@ -0,0 +1,46 @@
+// what is diff. between =, ==, ===
+
+//= :- assignable operator
+// == :- check the values same or not
+// === :- check the values as well as its dataType also
+
+// const b = 4;
+
+// const c = "4";
+
+// console.log(b == c);
+
+// const d = 4;
+
+// const e = 4;
+
+// console.log(d === e);
+
+// >
+//<
+
+// let text1 = 20;
+// let text2 = 5;
+// let result = text1 > text2;
+
+// console.log("result :>> ", result);
+
+// let text1 = "A";
+// let text2 = "B";
+// let result = text1 < text2;
+
+// console.log("result :>> ", result);
+
+// const a = 10;
+
+// const b = 10;
+
+// console.log("a>b :>> ", a <= b);
+
+// const z = 10;
+
+// console.log("object :>> ", z !== 11);
+
+// let x = 5;
+
+// console.log("x !== 5	 :>> ", x !== "5");
--- operator.js
+++ operator.js
@@ -50,3 +50,49 @@
 
 // const bb = firstName - lastName;
 // console.log("bb :>> ", bb); //Nan => Not a number
+
+// //multiplication
+
+// const a = 9;
+// const b = 10;
+
+// const c = a * b;
+
+// console.log("c :>> ", c);
+
+// const x = "asdfg";
+// const y = 100;
+
+// const z = x * y;
+// console.log("z :>> ", z);
+
+// let x = "100" / "50";
+
+// console.log("x :>> ", x);
+
+// //Increment
+// let a = 10;
+
+// a++;
+
+// console.log("a :>> ", a);
+
+// //Decrement
+// let b = 10;
+
+// b--;
+
+// console.log("b :>> ", b);
+
+// const navneet = 99;
+
+// navneet++;
+
+// console.log("navneet :>> ", navneet);
+
+// let x = 10;
+
+// let y = 20;
+// x += y; // x= x+y
+
+// console.log("x :>> ", x);
