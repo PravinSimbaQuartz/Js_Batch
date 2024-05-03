af723274f2181dc408127b66108141143bedec05
Pravin Patekar <pravinpatekar1999@gmail.com>
2024-05-03 13:06:57 +0530

    updated code

---
+++ array.js
@@ -0,0 +1,62 @@
+// // const abc = [1, 2, 3, 4, 5, 6, 7, 8];
+// // // console.log("abc.length :>> ", abc.length);
+// // // console.log("abc[0] :>> ", abc[10]);
+
+// // //length => 1 - 8
+// // //index => 0 - 7
+
+// // const days = ["sun", "mon", "Tues", "wednes"];
+
+// // const cars = []; //empty array
+
+// // // cars[0] = "Duster";
+// // // cars[1] = "BMW";
+// // // cars[4] = "Nano";
+
+// // // console.log("cars :>> ", cars[4]);
+
+// // //
+// // // const motors = new Array("Saab", "Volvo", "BMW");
+// // // console.log("motors :>> ", motors);
+
+// // //"sun", "mon", "Tues", "wednes"
+
+// // // const array = new Array("sun", "mon", "Tues", "wednes");
+
+// // // console.log("dfghjk :>> ", array);
+
+// // const array = ["sun", "mon", "Tues", "wednes"];
+
+// // array[0] = "saturday";
+// // array[3] = "friday";
+// // array[10] = "thursday";
+// // array[12] = 10;
+// // array[13] = true;
+
+// // console.log("dfghjk :>> ", array);
+
+// // array methods
+
+// const num = [7, 8, 9, 3, 4, 1, 2];
+
+// // console.log("num.sort() :>> ", num.sort());
+// // console.log("num/length :>> ", num.length);
+// // console.log("num.pop() :>> ", num.pop());
+// // console.log("num.push(5) :>> ", num.push(5, 45, 89, "asdfghjk"));
+// // console.log("num.unshift(29) :>> ", num.unshift(29));
+// // console.log("num.shift() :>> ", num.shift());
+// // console.log("num.toString() :>> ", num.toString());
+// // console.log("num[2] :>> ", num[2]);
+
+// console.log("num :>> ", num);
+
+// // const myGirls = ["Cecilie", "Lone"];
+// // const myBoys = ["Emil", "Tobias", "Linus"];
+
+// // const myChildren = myBoys.concat(myGirls);
+
+// // console.log("myChildren :>> ", myChildren);
+
+// const array = [1, 2, 3, [4, 5, 6, [7, 8, [9]]]];
+
+// console.log("array.flat() :>> ", array.flat());
